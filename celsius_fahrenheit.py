c6aa842ad973bb5bb860f8a30f107036fb74bde5
Ubuntu <ubuntu@ip-172-31-84-227.ec2.internal>
2020-12-19 01:38:58 +0000

    celsius_fahrenheit.py

---
+++ celsius_fahrenheit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# created by: Ryan Walsh
+# created on: December 2020
+# this program has a function that converts celsius to fahrenheit
+
+def fahrenheit():
+    # this program has a function that converts celsius to fahrenheit
+
+    # input, process & output
+    while True:
+        temp_celsius = input("Enter a temperature (°C): ")
+        print("\n", end="")
+        try:
+            temp_celsius = int(temp_celsius)
+            temp_fahrenheit = (9/5) * temp_celsius + 32
+            print("{0}°C is equal to {1}°F".format(temp_celsius,
+                                                   temp_fahrenheit))
+            break
+        except Exception:
+            print("{0} is not an integer. Please enter an integer."
+                  .format(temp_celsius))
+            print("\n", end="")
+
+
+def main():
+    # this function just calls other functions
+
+    # call function
+    fahrenheit()
+
+
+if __name__ == "__main__":
+    main()
